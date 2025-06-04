ee10d62d153126a38c04ac7a7e1559a8bd2f5bb3
QBicMC <173728298+QBicMC@users.noreply.github.com>
2025-06-03 21:28:04 -0400

    Censor more stuff

--- jbcensor/content.js
+++ jbcensor/content.js
@@ -1,6 +1,9 @@
 const censoredWords = [
   { base: "job", variants: ["job", "jobs", "jobless"], censorIndex: 1 },
-  { base: "work", variants: ["work", "works", "worked", "working", "worker", "workplace"], censorIndex: 1 }
+  { base: "work", variants: ["work", "works", "worked", "working", "worker", "workplace"], censorIndex: 1 },
+  { base: "apple bagger", variants: ["bagger", "bagga", "apple"], censorIndex: 1 },
+  { base: "ticket tracker", variants: ["ticket", "tracker", "tracka"], censorIndex: 1 },
+  { base: "a-bagger", variants: ["a-bagga", "t-tracker", "t-tracka"], censorIndex: 3 },
 ];
 
 function censorText(node) {
